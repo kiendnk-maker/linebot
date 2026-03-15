4639885dfe9f0123b415239bcd4b9fe3d4b9f339
kiendnk <kiendnk@gmail.com>
2026-03-16 05:24:37 +0800

    Surgical fix for unterminated f-string at line 1380

--- main.py
+++ main.py
@@ -2105,3 +2105,4 @@ async def process_event(event: MessageEvent) -> None:
 # Fix Syntax & Enable QR: Mon Mar 16 04:52:01 CST 2026
 # Fix Syntax & Enable QR: Mon Mar 16 05:15:55 CST 2026
 # Syntax fix for line 1380 - Mon Mar 16 05:21:46 CST 2026
+# Surgical Fix Syntax line 1380 - Mon Mar 16 05:24:37 CST 2026
